58309653b1d4afa56e2e68de1d79fc68db12056a
AdelCSE <aa.mokadem@esi-sba.dz>
2023-12-27 23:35:35 +0100

    init miro app

---
+++ src/app/api/redirect/route.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest } from "next";
+import { redirect } from "next/navigation";
+import initMiroAPI from "../../../utils/initMiroAPI";
+
+// handle redirect with code and exchange it for the access token
+export async function GET(request: NextApiRequest) {
+  const { miro, userId } = initMiroAPI();
+
+  // Make sure the code is in query parameters
+  const code = request.nextUrl.searchParams.get("code");
+  if (typeof code !== "string") {
+    redirect("/?missing-code");
+  }
+
+  try {
+    await miro.exchangeCodeForAccessToken(userId, code);
+  } catch (error) {
+    redirect("/?error");
+  }
+  redirect(`/`);
+}
